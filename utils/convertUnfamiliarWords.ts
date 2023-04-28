a83b9b93a20b446ad239dc94255d29589cfb3ef3
xyytgae <xyytgae@yahoo.co.jp>
2023-04-28 23:40:13 +0900

    feat: #10 🎸 プロンプト文言修正

--- utils/convertUnfamiliarWords.ts
+++ utils/convertUnfamiliarWords.ts
@@ -3,5 +3,5 @@ import { FormInput } from '../types/formInput'
 export const convertUnfamiliarWords = (formInput: FormInput): string => {
   const { unfamiliarWords, metaphor } = formInput
   const joinedWords = unfamiliarWords.map((word) => word.value).join('と')
-  return `${joinedWords}の違いを${metaphor}で例えて`
+  return `${joinedWords}の違いを簡潔に${metaphor}で例えて`
 }
